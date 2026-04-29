4dfba7d602bc585d748c7e93d9e57c00ca60c2b8
dhivyadarshini2305 <dhividharshini2305@gmail.com>
2026-04-29 19:55:15 +0530

    Create data.js

---
+++ data.js
@@ -0,0 +1,7 @@
+const dengueData = [
+    {year: 2019, cases: 50000, deaths: 100},
+    {year: 2020, cases: 20000, deaths: 50},
+    {year: 2021, cases: 40000, deaths: 80},
+    {year: 2022, cases: 70000, deaths: 120},
+    {year: 2023, cases: 90000, deaths: 150}
+];
